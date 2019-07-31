f1bdb80556a25ab944e25254c12ac969280cb5a5
Saad <saad94@live.com>
2019-07-31 11:55:45 +0500

    first commit

---
+++ TutorialTeacher/Action Filters/ActivityLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialTeacher.Action_Filters
+{
+    public class ActivityLog : ActionFilterAttribute, IExceptionFilter
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string message = "\n" + filterContext.ActionDescriptor.ControllerDescriptor.ControllerName +
+                 " -> " + filterContext.ActionDescriptor.ActionName + " -> OnActionExecuting \t- " +
+                 DateTime.Now.ToString() + "\n";
+       
+            logWrite(message);
+        }
+
+        public void logWrite(string message)
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/Data/Data.txt"), message);
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            string message = filterContext.RouteData.Values["controller"].ToString() + " => "
+                  + filterContext.RouteData.Values["action"].ToString() +
+                  " => " + filterContext.Exception.Message + "\t -" + DateTime.Now.ToString() + "\n";
+            logWrite(message);
+        }
+    }
+}
